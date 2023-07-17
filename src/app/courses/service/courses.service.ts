d2749b83c10481428ff1d1cfb5d713c2051c2a75
fagneraureliano <fagner2aureliano@gmail.com>
2023-07-17 18:39:49 -0300

    :sparkles: impl update service reusing course form

--- src/app/courses/service/courses.service.ts
+++ src/app/courses/service/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay, first } from 'rxjs';
+import { Observable, delay, first, tap } from 'rxjs';
 
 import { Course } from '../model/course.interface';
 
@@ -13,12 +13,27 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   list(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.API).pipe(first());
-  }
-  save(record: Partial<Course>): Observable<Course> {
-    return this.http.post<Course>(this.API, record).pipe(first());
+    return this.http.get<Course[]>(this.API).pipe(
+      first()
+      // delay(2000), // simula tempo de entrega de resposta
+      // tap((res) => console.log(res))
+    );
   }
   findById(id: string): Observable<Course> {
     return this.http.get<Course>(`${this.API}/${id}`).pipe(first());
+  }
+  save(record: Partial<Course>): Observable<Course> {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+  private create(record: Partial<Course>): Observable<Course> {
+    return this.http.post<Course>(this.API, record).pipe(first());
+  }
+  private update(record: Partial<Course>): Observable<Course> {
+    return this.http
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
   }
 }
